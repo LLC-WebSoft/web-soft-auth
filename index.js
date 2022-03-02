ca96b75a9e842cde1e4cc2c9bf24c4ccd3f54eb4
DanilSord <kincharov99@gmail.com>
2022-03-02 17:03:02 +0700

    rework: move recieveBody method to utils.js

--- index.js
+++ index.js
@@ -1,1 +1,102 @@
-require('./lib/server');
+const { Server } = require('./lib/server');
+const { Auth } = require('./lib/auth');
+
+const modules = {
+  auth: {
+    schema: {
+      register: {
+        public: true,
+        params: {
+          required: ['username', 'password'],
+          properties: {
+            username: {
+              type: 'string'
+            },
+            password: {
+              type: 'string'
+            }
+          }
+        },
+        result: {
+          required: ['username', 'role', 'createdTime'],
+          properties: {
+            username: {
+              type: 'string'
+            },
+            role: {
+              type: 'string'
+            },
+            createdTime: {
+              type: 'string'
+            }
+          }
+        }
+      },
+      login: {
+        public: true,
+        params: {
+          required: ['username', 'password'],
+          properties: {
+            username: {
+              type: 'string'
+            },
+            password: {
+              type: 'string'
+            }
+          }
+        },
+        result: {
+          required: ['username', 'role', 'createdTime'],
+          properties: {
+            username: {
+              type: 'string'
+            },
+            role: {
+              type: 'string'
+            },
+            createdTime: {
+              type: 'string'
+            }
+          }
+        }
+      },
+      logout: {
+        public: false
+      },
+      changePassword: {
+        public: false,
+        params: {
+          required: ['username', 'oldPassword', 'newPassword'],
+          properties: {
+            username: {
+              type: 'string'
+            },
+            oldPassword: {
+              type: 'string'
+            },
+            newPassword: {
+              type: 'string'
+            }
+          }
+        },
+        result: {
+          required: ['username', 'role', 'createdTime'],
+          properties: {
+            username: {
+              type: 'string'
+            },
+            role: {
+              type: 'string'
+            },
+            createdTime: {
+              type: 'string'
+            }
+          }
+        }
+      }
+    },
+    Module: Auth
+  }
+};
+
+const server = new Server(modules, { port: 80, host: 'localhost' });
