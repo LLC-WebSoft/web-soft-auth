3049bece683e193b830d99c32cd31ecf781c8840
DonVietnam <don.vietnam.js@gmail.com>
2022-08-20 12:10:47 +0700

    introspection: add getErrors method to module class

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const cert = fs.readFileSync('./certs/localhost.crt');
 
 const start = async () => {
   try {
-    const server = new Server({ host: 'localhost', port: 443, cors: false, key, cert, secure: true });
+    const server = new Server({ host: '0.0.0.0', port: 443, cors: false, key, cert, secure: true });
     server.start(modules);
   } catch (error) {
     logger.fatal(error);
